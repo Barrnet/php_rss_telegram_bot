9111281b6205c472ddaf8cf5c15432f7ba41a22a
Barrnet <barrnet@videogamezone.eu>
2017-08-05 02:47:58 +0200

    Create bot_config.php

---
+++ bot_config.php
@@ -0,0 +1,23 @@
+<?php 
+/* 
+**********************************
+**** Variabili API Telegram ******
+**********************************
+*/
+/* Token API Telegram. Da richiere a @BotFather */ 
+$token = 'XXXXXXXXX:XXXXXXXXXXXXXXXXXXXXXXXXXX-XXXXXXXX';
+
+/* Chat a cui spedire i messaggi */
+$chat = '@XXX'; 
+/* Feed RSS da cui prendere i valori */
+$rss = 'https://yourfeed.com';
+
+/* File in cui salvare i log */
+$log_file = 'channel_bot.log';
+
+/* File in cui salvare il PID */
+$pid_file = 'bot.pid';
+
+/* Attesa tra un ciclo e l'altro */
+$attesa = 60;
+?>
